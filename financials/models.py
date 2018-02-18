9a28f2061a5bffd42283b29c3acf19e4e21c57ba
Hassan Bayloun <hassan.bayloun@gmail.com>
2018-02-18 14:51:34 +0200

    commit:

--- financials/models.py
+++ financials/models.py
@@ -3,13 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Financial(models.Model):
-    status_choices = (
-        ("Undelivered", 'Undelivered'),
-        ("Delivered with driver", 'Delivered with driver'),
-        ("Delivered with company", 'Delivered with company'),
-        ("Paid to seller", 'Paid to seller'),
-    )
-    status = models.CharField(max_length=20, choices=status_choices)
+    status = models.CharField(max_length=20)
     description = models.TextField()
 
     def __str__(self):
