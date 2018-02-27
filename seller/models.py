b7438cea178976a01959e2955d1702a425e6b42c
Hassan Bayloun <hassan.bayloun@gmail.com>
2018-02-27 12:42:38 +0200

    Seller table migrations

--- seller/models.py
+++ seller/models.py
@@ -7,6 +7,8 @@ class Seller(models.Model):
     contact_name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=20)
     operations_phone_number = models.CharField(max_length=20)
+    lebanon_rate = models.FloatField(null=True, blank=True)
+    beirut_rate = models.FloatField(null=True, blank=True)
 
     def __str__(self):
         return self.business_name
